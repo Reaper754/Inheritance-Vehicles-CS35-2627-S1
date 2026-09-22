13bc4f1580e485a41116d441148aef58c42b5cea
Reaper754 <MasonT019@stemcollegiate.ca>
2026-09-22 10:31:08 -0600

    almost complete

---
+++ main.py
@@ -0,0 +1,11 @@
+from vehicles import Vehicle
+from vehicles import Car
+from vehicles import Motorcycle
+
+vehicle = Vehicle("NA", "NA", "NA")
+car = Car("Honda", "Civic", 100, 2700)
+motorcycle = Motorcycle("Yamaha", "YZF-R1", 80, 448)
+
+car.traffic()
+motorcycle.traffic()
+vehicle.traffic()
--- vehicles.py
+++ vehicles.py
@@ -5,12 +5,41 @@ class Vehicle:
         self.speed = speed
         self.fuel = "gas"
 
+    def inspect(self):
+        print(f"This is a {self.make} {self.model} going {self.speed}km/h using {self.fuel}.")
+
+    def refuel(self):
+        print(f"The {self.make} {self.model} refuels at a gas station with {self.fuel}.")
+
+    def traffic(self):
+        print(f"The {self.make} {self.model} is stuck in traffic.")
+
 class Car(Vehicle):
     def __init__(self, make, model, speed, weight):
         super().__init__(make, model, speed)
         self.weight = weight
 
+    def inspect(self):
+        super().inspect()
+        print(f"It weighs {self.weight} pounds.")
+
+    def drift(self):
+        print(f"The {self.make} {self.model} hits a sick drift.")
+
+    def traffic(self):
+        print(f"The {self.make} {self.model} honks their horn in response to traffic. BEEP BEEP!")
+
 class Motorcycle(Vehicle):
     def __init__(self, make, model, speed, weight):
         super().__init__(make, model, speed)
         self.weight = weight
+
+    def inspect(self):
+        super().inspect()
+        print(f"It weighs {self.weight} pounds.")
+
+    def wheelie(self):
+        print(f"The {self.make} {self.model} pops a wheelie.")
+
+    def traffic(self):
+        print(f"The {self.make} {self.model} lane splits to completely avoid traffic.")
